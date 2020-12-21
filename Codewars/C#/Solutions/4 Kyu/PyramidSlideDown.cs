275fe738c319b824481f1f5dfc52034ac78e6273
Vi Tiet <vitiet.programmer@gmail.com>
2020-12-21 04:14:49 -0500

    Add Solutions

---
+++ Codewars/C#/Solutions/4 Kyu/PyramidSlideDown.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Codewars.Solutions._4_Kyu
+{
+    /// Lyrics...
+    /// Pyramids are amazing! Both in architectural and mathematical sense.If you have a computer, you
+    /// can mess with pyramids even if you are not in Egypt at the time. For example, let's consider the 
+    /// following problem. Imagine that you have a pyramid built of
+    /// numbers, like this one here:
+    ///
+    ///           /3/
+    ///          \7\ 4 
+    ///         2 \4\ 6 
+    ///        8 5 \9\ 3
+    ///
+    /// Here comes the task...
+    /// Let's say that the 'slide down' is the maximum sum of consecutive numbers from the top to the bottom 
+    /// of the pyramid.
+    /// As you can see, the longest 'slide down' is 3 + 7 + 4 + 9 = 23
+    ///
+    ///
+    /// Your task is to write a function longestSlideDown (in ruby/crystal/julia: longest_slide_down) that
+    /// takes a pyramid representation as argument and returns its' largest 'slide down'. For example:
+    ///
+    /// LongestSlideDown(new[] { new[] {3}, new[] { 7, 4 }, new[] { 2, 4, 6 }, new[] { 8, 5, 9, 3 } }); => 23
+    ///
+    /// By the way...
+    /// My tests include some extraordinarily high pyramids so as you can guess, brute-force method 
+    /// is a bad idea unless you have a few centuries to waste. You must come up with something more clever 
+    /// than that.
+    ///
+    /// (c) This task is a lyrical version of the Problem 18 and/or Problem 67 on ProjectEuler.
+
+    class PyramidSlideDown
+    {
+        public static int Solution(int[][] pyramid)
+        {
+            // find from bottom up
+            for (int i = pyramid.GetLength(0) - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < pyramid[i].GetLength(0); j++)
+                {
+                    int left = pyramid[i][j] + pyramid[i + 1][j];
+                    int right = pyramid[i][j] + pyramid[i + 1][j + 1];
+                    pyramid[i][j] = Math.Max(left, +right);
+                }
+            }
+
+            return pyramid[0][0];
+        }
+    }
+}
---
+++ Codewars/C#/Solutions/5 Kyu/Josephus Permutation.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codewars.Solutions._5_Kyu
+{
+    ///This problem takes its name by arguably the most important event in the life of the ancient 
+    ///historian Josephus: according to his tale, he and his 40 soldiers were trapped in a cave by 
+    ///the Romans during a siege.
+    ///
+    ///Refusing to surrender to the enemy, they instead opted for mass suicide, with a twist: they 
+    ///formed a circle and proceeded to kill one man every three, until one last man was left (and 
+    ///that it was supposed to kill himself to end the act).
+    ///
+    ///Well, Josephus and another man were the last two and, as we now know every detail of the 
+    ///story, you may have correctly guessed that they didn't exactly follow through the original 
+    ///idea.
+    ///
+    ///You are now to create a function that returns a Josephus permutation, taking as parameters 
+    ///the initial array/list of items to be permuted as if they were in a circle and counted out 
+    ///every k places until none remained.
+    ///
+    ///Tips and notes: it helps to start counting from 1 up to n, instead of the usual range 0..n-1; 
+    ///k will always be >=1.
+    ///
+    ///For example, with n=7 and k=3 josephus(7,3) should act this way.
+    ///
+    ///[1,2,3,4,5,6,7] - initial sequence
+    ///[1,2,4,5,6,7] => 3 is counted out and goes into the result [3]
+    ///[1,2,4,5,7] => 6 is counted out and goes into the result [3,6]
+    ///[1,4,5,7] => 2 is counted out and goes into the result [3,6,2]
+    ///[1,4,5] => 7 is counted out and goes into the result [3,6,2,7]
+    ///[1,4] => 5 is counted out and goes into the result [3,6,2,7,5]
+    ///[4] => 1 is counted out and goes into the result [3,6,2,7,5,1]
+    ///[] => 4 is counted out and goes into the result [3,6,2,7,5,1,4]
+    ///So our final result is:
+    ///
+    ///josephus([1,2,3,4,5,6,7],3)==[3,6,2,7,5,1,4]
+    ///For more info, browse the Josephus Permutation page on wikipedia; related kata: Josephus 
+    ///Survivor.This problem takes its name by arguably the most important event in the life of the 
+    ///ancient historian Josephus: according to his tale, he and his 40 soldiers were trapped in a 
+    ///cave by the Romans during a siege.
+    ///
+    ///Refusing to surrender to the enemy, they instead opted for mass suicide, with a twist: they 
+    ///formed a circle and proceeded to kill one man every three, until one last man was left (and 
+    ///that it was supposed to kill himself to end the act).
+    ///
+    ///Well, Josephus and another man were the last two and, as we now know every detail of the story, 
+    ///you may have correctly guessed that they didn't exactly follow through the original idea.
+    ///
+    ///You are now to create a function that returns a Josephus permutation, taking as parameters the 
+    ///initial array/list of items to be permuted as if they were in a circle and counted out every k 
+    ///places until none remained.
+    ///
+    ///Tips and notes: it helps to start counting from 1 up to n, instead of the usual range 0..n-1; 
+    ///k will always be >=1.
+    ///
+    ///For example, with n=7 and k=3 josephus(7,3) should act this way.
+    ///
+    ///[1,2,3,4,5,6,7] - initial sequence
+    ///[1,2,4,5,6,7] => 3 is counted out and goes into the result [3]
+    ///[1,2,4,5,7] => 6 is counted out and goes into the result [3,6]
+    ///[1,4,5,7] => 2 is counted out and goes into the result [3,6,2]
+    ///[1,4,5] => 7 is counted out and goes into the result [3,6,2,7]
+    ///[1,4] => 5 is counted out and goes into the result [3,6,2,7,5]
+    ///[4] => 1 is counted out and goes into the result [3,6,2,7,5,1]
+    ///[] => 4 is counted out and goes into the result [3,6,2,7,5,1,4]
+    ///So our final result is:
+    ///
+    ///josephus([1,2,3,4,5,6,7],3)==[3,6,2,7,5,1,4]
+    ///For more info, browse the Josephus Permutation page on wikipedia; related kata: Josephus Survivor.
+    
+    class JosephusPermutation
+    {
+        public static List<object> Solution(List<object> items, int k)
+        {
+            var l = new List<object>();
+            int count = 0;
+
+            while (items.Count > 0)
+            {
+                count = (count + k - 1) % items.Count;
+                l.Add(items[count]);
+                items.RemoveAt(count);
+            }
+
+            return l;
+        }
+    }
+}
